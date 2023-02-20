3c15b700656293ab27e8165a37f3f2a1124459db
hosonishi <hosonishi@gmail.com>
2023-02-20 20:18:54 +0900

    separate dao

--- src/company.rs
+++ src/company.rs
@@ -6,14 +6,23 @@ use serde::{Deserialize, Serialize};
 use sqlx::mysql::MySqlPool;
 
 pub async fn find_companies(pool: MySqlPool) -> Json<Vec<Company>> {
-  let row: (i64, String,) = sqlx::query_as("SELECT id, name from companies")
-  .fetch_one(&pool).await.expect("error");
+  let companies = company_dao::select_companies(pool);
+  Json(companies.await)
+}
 
-  let companies = vec!(Company {
-      id: row.0,
-      name: row.1,
-  });
-  Json(companies)
+mod company_dao {
+  use sqlx::mysql::MySqlPool;
+  use crate::company::Company;
+
+  pub async fn select_companies(pool: MySqlPool) -> Vec<Company> {
+    let row: (i64, String,) = sqlx::query_as("SELECT id, name from companies")
+    .fetch_one(&pool).await.expect("error");
+
+    vec!(Company {
+        id: row.0,
+        name: row.1,
+    })
+  }
 }
 
 pub async fn create_company(
