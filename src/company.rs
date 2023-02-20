0c8ac4874a0094ed383018d4fc8b707fa902aaa1
hosonishi <hosonishi@gmail.com>
2023-02-20 20:39:34 +0900

    separate dao_impl

--- src/company.rs
+++ src/company.rs
@@ -4,25 +4,47 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use sqlx::mysql::MySqlPool;
+use crate::company::company_dao::CompanyDao;
 
 pub async fn find_companies(pool: MySqlPool) -> Json<Vec<Company>> {
-  let companies = company_dao::select_companies(pool);
+  let company_dao = company_dao_impl::CompanyDaoImpl{ pool };
+  let companies = company_dao.select_companies();
   Json(companies.await)
 }
 
 mod company_dao {
-  use sqlx::mysql::MySqlPool;
+  use async_trait::async_trait;
   use crate::company::Company;
 
-  pub async fn select_companies(pool: MySqlPool) -> Vec<Company> {
-    let row: (i64, String,) = sqlx::query_as("SELECT id, name from companies")
-    .fetch_one(&pool).await.expect("error");
-
-    vec!(Company {
-        id: row.0,
-        name: row.1,
-    })
+  #[async_trait]
+  pub trait CompanyDao {
+    async fn select_companies(&self) -> Vec<Company>;  
   }
+}
+
+mod company_dao_impl {
+  use async_trait::async_trait;
+  use sqlx::mysql::MySqlPool;
+  use crate::company::Company;
+  use crate::company::company_dao::CompanyDao;
+
+  pub struct CompanyDaoImpl {
+    pub pool: MySqlPool
+  }
+
+  #[async_trait]
+  impl CompanyDao for CompanyDaoImpl {
+    async fn select_companies(&self) -> Vec<Company> {
+      let row: (i64, String,) = sqlx::query_as("SELECT id, name from companies")
+      .fetch_one(&self.pool).await.expect("error");
+  
+      vec!(Company {
+          id: row.0,
+          name: row.1,
+      })
+    }  
+  }
+
 }
 
 pub async fn create_company(
